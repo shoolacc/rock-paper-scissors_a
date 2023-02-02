d82802232a0876b6b0fd952414ca6adc1378b168
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-02-02 11:01:12 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == player1_choice) {
+        basic.showString("Draw!")
+    } else {
+        if (receivedNumber < player1_choice) {
+            if (receivedNumber == 2 && player1_choice == 0) {
+                basic.showString("Win!")
+                game.setScore(1)
+                basic.showString("" + (game.score()))
+            } else {
+                basic.showString("Win!")
+                game.setScore(1)
+                basic.showString("" + (game.score()))
+            }
+        } else {
+            if (player1_choice == 2 && receivedNumber == 0) {
+                basic.showString("Lose")
+            } else {
+                basic.showString("Lose")
+            }
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    game.setScore(0)
+})
+input.onGesture(Gesture.Shake, function () {
+    player1_choice = randint(0, 2)
+    if (player1_choice == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        radio.sendNumber(0)
+    }
+    if (player1_choice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        radio.sendNumber(1)
+    }
+    if (player1_choice == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        radio.sendNumber(2)
+    }
+})
+let player1_choice = 0
+basic.showString("Shake to play")
+game.setScore(0)
+basic.forever(function () {
+    radio.setGroup(4)
+})
